7df18b4f4a74b12bf811685c6de7b9c53d723e62
Norman A. Luna <baciloferoz@gmail.com>
2023-04-27 20:42:31 -0600

    Wip: Front of cocibolca-app

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css'],
 })
-export class ProductCardComponent {
-  product = 'Producto X';
-  description = 'Lorem ipsum asbkabdkasjbfkhbadhfbakshbfhkabskbfkhsb';
+export class ProductCardComponent implements OnInit {
+  constructor() {}
+  supplier = 'Proveedor X';
+
+  ngOnInit() {}
 }
