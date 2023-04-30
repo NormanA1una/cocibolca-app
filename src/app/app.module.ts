fee9efef14512094189b08424ac78e150cb1651a
NormanA1una <baciloferoz@gmail.com>
2023-04-30 12:51:56 -0600

    implement Material and TailWindCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, ProductCardComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSlideToggleModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
